86e898973e19b66f8a41791606d6c9a030152ffa
brunocoelhosi <brunocoelho.si@outlook.com>
2026-04-10 16:17:06 -0300

    fix icon social media

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { MessageCircle } from "lucide-react";
+import { MessageCircleCheck } from "lucide-react";
 
 import Navbar from "./Home/navbar";
 import Hero from "./Home/hero";
@@ -24,7 +24,7 @@ function App() {
           href="https://wa.me/+5534996373011"
           className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-2xl hover:scale-110 transition z-50"
         >
-          <MessageCircle size={30} />
+          <MessageCircleCheck size={25} />
         </a>
       </div>
     </>
